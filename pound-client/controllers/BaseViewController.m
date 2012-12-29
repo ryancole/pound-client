4373c27ec29116070b6081455f5078fc8e46dcee
Ryan Cole <ryan@rycole.com>
2012-12-29 16:22:44 -0600

    adding in the compose message screen

--- pound-client/controllers/BaseViewController.m
+++ pound-client/controllers/BaseViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BaseViewController.h"
+#import "ComposeMessageViewController.h"
 
 @interface BaseViewController ()
 
@@ -26,13 +27,24 @@
     // create the compose button
     NSMutableArray *items = [[NSMutableArray alloc] init];
     [items addObject:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil]];
-    [items addObject:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:nil]];
+    [items addObject:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:@selector(composeMessageButtonPressed:)]];
     
     // add the compose button to the toolbar
     [toolbar setItems:items];
     
     // add the toolbar to this view's subview
     [self.view addSubview:toolbar];
+}
+
+- (void)composeMessageButtonPressed:(id)sender {
+    
+    ComposeMessageViewController *view = [[ComposeMessageViewController alloc] init];
+    
+    view.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    view.modalPresentationStyle = UIModalPresentationFormSheet;
+    
+    [self presentViewController:view animated:YES completion:nil];
+    
 }
 
 @end
---
+++ pound-client/controllers/ComposeMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComposeMessageViewController.h
+//  pound-client
+//
+//  Created by Ryan Cole on 12/29/12.
+//  Copyright (c) 2012 Ryan Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeMessageViewController : UIViewController
+
+@end
